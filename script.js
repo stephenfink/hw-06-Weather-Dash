054af1c0916dfbbaac546872a1445cdef03eae64
Stephen Fink <stephenfink8@gmail.com>
2020-08-27 16:51:31 -0500

    started planning and div tags for document

---
+++ script.js
@@ -0,0 +1,20 @@
+//The following will need to have coded
+
+    //a way to get the information from the data base for weather
+        //one for current day
+        //one for the next day 1
+        //one for the next day 2
+        //one for the next day 3
+        //one for the next day 4
+        //one for the next day 5
+
+    //This should have the ability to use the location of the search 
+        //storage it as a keyword of search to go back to the city
+        //use local storage to keep past searches
+    
+    //use the data from API to get following for each day
+        //the type of weather i.e. rain, clouds, etc
+        // the temp of weather
+        //humidity levels
+        // wind speed 
+        //UV index
