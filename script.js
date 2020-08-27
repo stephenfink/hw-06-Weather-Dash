bcd90eed39b0a6fdf7410cb57dea9854b56ec6fc
Stephen Fink <stephenfink8@gmail.com>
2020-08-27 18:01:07 -0500

    this is for the weather :

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 //The following will need to have coded
+//things for sure need to be in a var
+var place; //This is for current search
+var history; //this is for past search
+
+
+    //This should have the ability to use the location of the search 
+        //storage it as a keyword of search to go back to the city
+        //use local storage to keep past searches
+        //using JSON 
 
     //a way to get the information from the data base for weather
         //one for current day
@@ -8,13 +17,10 @@
         //one for the next day 4
         //one for the next day 5
 
-    //This should have the ability to use the location of the search 
-        //storage it as a keyword of search to go back to the city
-        //use local storage to keep past searches
     
     //use the data from API to get following for each day
         //the type of weather i.e. rain, clouds, etc
         // the temp of weather
         //humidity levels
         // wind speed 
-        //UV index
+        //UV index that changes color base on level
